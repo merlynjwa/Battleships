b49a0fb5fe0d2f13388dae8e35c5bdd4a4e04ad6
merlynjwa <merlyn.wa@gmail.com>
2022-08-13 14:01:25 +0000

    Take the user's ships' co-ordinates as input
    Take a string from the standard input of the terminal.
    This input will be used to parse the co-ordinates for the user's ships.

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ def main():
           "Carrier - 5, Battleship - 4, Cruiser - 3, Submarine - 3, Destroyer - 2\n\n"
           "Here's an example of how the input should be formatted:\n"
           "\"c4,up; a0,right; j9,down; e10,left; h7,down\"\n")
+    input_string = input("Enter your ships' co-ordinates:\n")
 
 
 main()

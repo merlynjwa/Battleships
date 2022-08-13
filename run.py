285978468a61e3de214b2f9c146f14482c2e4012
merlynjwa <merlyn.wa@gmail.com>
2022-08-13 10:19:22 +0000

    Add starting instructions when running the python script
    I've added instructions to be printed to the terminal telling the
    user that they need to enter the co-ordinates of their ships,
    giving them an example of the format that the input should be in.

--- run.py
+++ run.py
@@ -2,6 +2,11 @@ def main():
     """
     The beginning of the program
     """
+    print("Please select the co-ordinates for your ships\n"
+          "Enter the co-ordinates in the following order:\n"
+          "Carrier - 5, Battleship - 4, Cruiser - 3, Submarine - 3, Destroyer - 2\n\n"
+          "Here's an example of how the input should be formatted:\n"
+          "\"c4,up; a0,right; j9,down; e10,left; h7,down\"\n")
 
 
 main()

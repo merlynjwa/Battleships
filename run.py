594160d2d1892e66049764cf3ff14ba445297db2
merlynjwa <merlyn.wa@gmail.com>
2022-08-13 16:16:00 +0000

    Loop section for user's input
    If the user's input does not match the regex,
    then they should be prompted to enter it again.
    Thus the section prompting the user for input and checking it against
    the regex is placed inside an infinite loop.
    
    With further design, the loop will be broken out of when the input
    matches the regex.

--- run.py
+++ run.py
@@ -10,14 +10,15 @@ def main():
           "Carrier - 5, Battleship - 4, Cruiser - 3, Submarine - 3, Destroyer - 2\n\n"
           "Here's an example of how the input should be formatted:\n"
           "\"c4,up; a0,right; j9,down; e10,left; h7,down\"\n")
-    input_string = input("Enter your ships' co-ordinates:\n")
-    match = re.match(r"""([a-j](10|[0-9]),(up?|r(ight)?|d(own)?|l(eft)?);){5}\s*""",
-                     input_string,
-                     flags=re.I | re.M | re.X)
-    if match:
-        pass
-    else:
-        print("\nThe entered string could not be parsed as co-ordinates\n")
+    while True:
+        input_string = input("Enter your ships' co-ordinates:\n")
+        match = re.match(r"""([a-j](10|[0-9]),(up?|r(ight)?|d(own)?|l(eft)?);){5}\s*""",
+                         input_string,
+                         flags=re.I | re.M | re.X)
+        if match:
+            pass
+        else:
+            print("\nThe entered string could not be parsed as co-ordinates\n")
 
 
 main()

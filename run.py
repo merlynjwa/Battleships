5525cd89075f17813d86446271f18219d2bff59b
merlynjwa <merlyn.wa@gmail.com>
2022-08-15 11:16:04 +0000

    Remove prompts asking for ship co-ordinates from main function
    I've decided to move the starting prompts for the user to input
    their ship's positions on the board. I'm going to move it in to the
    Game class.
    
    The idea is then to have the Game class construct the boards for
    both the player and the cpu and for it to contain the logic for
    the rest of the game.

--- run.py
+++ run.py
@@ -5,24 +5,7 @@ def main():
     """
     The beginning of the program
     """
-    print("Please select the co-ordinates for your ships\n"
-          "Enter the co-ordinates in the following order:\n"
-          "Carrier - 5, Battleship - 4, Cruiser - 3, Submarine - 3, Destroyer - 2\n\n"
-          "Here's an example of how the input should be formatted:\n"
-          "\"c4,up; a0,right; j9,down; e10,left; h7,down\"\n")
-    while True:
-        input_string = input("Enter your ships' co-ordinates:\n")
-        match = re.match(r"""(?P<carrier>[a-j](10|[0-9]),(up?|r(ight)?|d(own)?|l(eft)?));\s*
-                             (?P<battleship>[a-j](10|[0-9]),(up?|r(ight)?|d(own)?|l(eft)?));\s*
-                             (?P<cruiser>[a-j](10|[0-9]),(up?|r(ight)?|d(own)?|l(eft)?));\s*
-                             (?P<submarine>[a-j](10|[0-9]),(up?|r(ight)?|d(own)?|l(eft)?));\s*
-                             (?P<destroyer>[a-j](10|[0-9]),(up?|r(ight)?|d(own)?|l(eft)?));?\s*""",
-                         input_string,
-                         flags=re.I | re.M | re.X)
-        if match:
-            ship_strings = match.groupdict()
-        else:
-            print("\nThe entered string could not be parsed as co-ordinates\n")
+    GameBoard()
 
 
 class Game:
